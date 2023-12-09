865e18aaf408613e5768b63826b12b3160448788
MigVarona <146460162+MigVarona@users.noreply.github.com>
2023-12-09 15:02:10 +0100

    update

--- js/inspiration.js
+++ js/inspiration.js
@@ -2,18 +2,18 @@ const accessKey = '44Yh3j04VI0h73noLdqOWPkEjsTRqP17zwhwsVNc7W4';
 const apiUrl = 'https://api.unsplash.com/photos/random';
 const photoContainer = document.getElementById('photo-container');
 const categorySelect = document.getElementById('category');
+let currentPage = 1;
 
 function loadRandomPhotos() {
     const category = categorySelect.value;
     const numberOfPhotos = 3;
 
-    const url = `${apiUrl}?client_id=${accessKey}&query=${category}&count=${numberOfPhotos}`;
+    const url = `${apiUrl}?client_id=${accessKey}&query=${category}&count=${numberOfPhotos}&page=${currentPage}`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const photosHtml = data.map(photo => {
-                // Extract artist's name from the user object
                 const artistName = photo.user.name;
                 return `
                     <div class="col-md-4 mb-4">
@@ -30,6 +30,18 @@ function loadRandomPhotos() {
         });
 }
 
+function loadNextPage() {
+    currentPage++;
+    loadRandomPhotos();
+}
+
 // Trigger the function when the page loads or when the category is changed
 loadRandomPhotos();
-categorySelect.addEventListener('change', loadRandomPhotos);
+categorySelect.addEventListener('change', () => {
+    currentPage = 1; // Reset page when category changes
+    loadRandomPhotos();
+});
+
+// Implementación básica de botón para cargar la siguiente página
+const loadMoreButton = document.getElementById('load-more-button');
+loadMoreButton.addEventListener('click', loadNextPage);
