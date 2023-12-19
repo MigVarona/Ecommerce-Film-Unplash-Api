1d8ab908ce3b02a272cf53fe0f677a23233f2549
MigVarona <146460162+MigVarona@users.noreply.github.com>
2023-12-19 12:00:34 +0100

    foto

--- js/film.test.js
+++ js/film.test.js
@@ -1,25 +1,58 @@
 // film.test.js
+const { fetchData, createCardsAndModals, addToCart } = require('./film');
 
-// Importa la función que deseas probar
-const { createCardsAndModals, addToCart } = require('./film'); // Asegúrate de ajustar la ruta según tu estructura de carpetas
+// Mock para la función fetch
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ /* mock data aquí */ }),
+  })
+);
 
-// Puedes agregar más imports según sea necesario
+// Mock para jQuery
+global.$ = jest.fn();
 
-// Aquí empiezan las pruebas
+// Mock para localStorage y window.location
+const localStorageMock = {
+  setItem: jest.fn(),
+};
+
+Object.defineProperty(global, 'localStorage', { value: localStorageMock });
+
+Object.defineProperty(window, 'location', {
+  value: { href: '' },
+});
+
+describe('fetchData', () => {
+  test('debería cargar datos de manera asíncrona', async () => {
+    const data = await fetchData();
+    // Asegúrate de que los datos sean los esperados
+    expect(data).toEqual(/* mock data esperado */);
+  });
+});
+
 describe('createCardsAndModals', () => {
-  test('debería agregar tarjetas al contenedor', () => {
-    // Tu lógica de prueba aquí
+  test('debería crear tarjetas y modales', () => {
+    const mockData = [{ /* datos de ejemplo */ }];
+    createCardsAndModals(mockData);
+
+    // Asegúrate de que se hayan llamado a las funciones adecuadas
+    expect($.fn.append).toHaveBeenCalled(); // jQuery append
   });
 
-  test('debería agregar eventos de clic a las tarjetas', () => {
-    // Tu lógica de prueba aquí
-  });
+  // Puedes añadir más pruebas según sea necesario
 });
 
 describe('addToCart', () => {
   test('debería almacenar el ítem seleccionado y redirigir a checkout.html', () => {
-    // Tu lógica de prueba aquí
+    const mockData = [{ /* datos de ejemplo */ }];
+    const index = 0;
+
+    addToCart(index);
+
+    // Verifica que localStorage.setItem se llame con los argumentos correctos
+    expect(localStorage.setItem).toHaveBeenCalledWith('selectedItem', JSON.stringify(mockData[index]));
+
+    // Verifica que window.location.href se establezca en 'checkout.html'
+    expect(window.location.href).toBe('checkout.html');
   });
 });
-
-
