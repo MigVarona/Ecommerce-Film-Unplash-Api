d3527c279bcd3fc3ab7516e1c0bb88425458f327
MigVarona <146460162+MigVarona@users.noreply.github.com>
2023-12-19 09:47:37 +0100

    push

--- js/main.js
+++ js/main.js
@@ -1,95 +1,115 @@
+// Evento que se ejecuta cuando se ha cargado completamente el DOM
 document.addEventListener("DOMContentLoaded", function () {
+    // Clave de acceso para la API de Unsplash
     const accessKey = '44Yh3j04VI0h73noLdqOWPkEjsTRqP17zwhwsVNc7W4';
-    
+
+    // Función para agregar una imagen a una tarjeta
     function addImage(photo, cardIndex) {
+        // Seleccionar la tarjeta según el índice proporcionado
         const card = document.getElementsByClassName('card')[cardIndex];
 
-   
-
+        // Crear un elemento de imagen
         const imgElement = document.createElement("img");
+
+        // Calcular la altura proporcional basándose en el ancho deseado
         const desiredWidth = 350;
         const calculatedHeight = (desiredWidth / photo.width) * photo.height;
 
+        // Establecer atributos de la imagen
         imgElement.src = `${photo.urls.regular}&w=${desiredWidth}&h=${calculatedHeight}`;
         imgElement.alt = photo.alt_description;
 
+        // Manejar el evento de error de carga de la imagen
         imgElement.addEventListener('error', (event) => {
             console.error(`Error al cargar la imagen: ${event.target.src}`);
         });
 
+        // Seleccionar el cuerpo de la tarjeta
         const cardBody = card.querySelector('.card-body');
 
+        // Verificar si se encontró el elemento con la clase "card-body"
         if (!cardBody) {
             console.error('No se encontró el elemento con la clase "card-body" en el índice:', cardIndex);
             return;
         }
 
+        // Insertar la imagen al principio del cuerpo de la tarjeta
         cardBody.insertBefore(imgElement, cardBody.firstChild);
     }
 
-    // Obtener fotos aleatorias de tu colección de Unsplash
+    // Obtener fotos aleatorias de la colección "RPfaQ1JJOeM" de Unsplash
     fetch('https://api.unsplash.com/collections/RPfaQ1JJOeM/photos?client_id=' + accessKey + '&order_by=')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Error HTTP! Estado: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        data.forEach((photo, index) => {
-            addImage(photo, index);
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error HTTP! Estado: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Iterar sobre las fotos y agregarlas a las tarjetas
+            data.forEach((photo, index) => {
+                addImage(photo, index);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener fotos de la colección:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error al obtener fotos de la colección:', error);
-    });
 });
 
-// Segunda función para agregar fotos en las tarjetas de film.
-
+// Segundo evento para agregar fotos a las tarjetas de film
 document.addEventListener("DOMContentLoaded", function () {
+    // Clave de acceso para la API de Unsplash
     const accessKey = '44Yh3j04VI0h73noLdqOWPkEjsTRqP17zwhwsVNc7W4';
-    
+
+    // Función para agregar una imagen a una tarjeta de film
     function addImage(photo, cardIndex) {
+        // Seleccionar la tarjeta de film según el índice proporcionado
         const card = document.getElementsByClassName('film')[cardIndex];
 
-   
-
+        // Crear un elemento de imagen
         const imgElement = document.createElement("img");
+
+        // Calcular la altura proporcional basándose en el ancho deseado
         const desiredWidth = 350;
         const calculatedHeight = (desiredWidth / photo.width) * photo.height;
 
+        // Establecer atributos de la imagen
         imgElement.src = `${photo.urls.regular}&w=${desiredWidth}&h=${calculatedHeight}`;
         imgElement.alt = photo.alt_description;
 
+        // Manejar el evento de error de carga de la imagen
         imgElement.addEventListener('error', (event) => {
             console.error(`Error al cargar la imagen: ${event.target.src}`);
         });
 
+        // Seleccionar el cuerpo de la tarjeta
         const cardBody = card.querySelector('.card-body');
 
+        // Verificar si se encontró el elemento con la clase "card-body"
         if (!cardBody) {
             console.error('No se encontró el elemento con la clase "card-body" en el índice:', cardIndex);
             return;
         }
 
+        // Insertar la imagen al principio del cuerpo de la tarjeta
         cardBody.insertBefore(imgElement, cardBody.firstChild);
     }
 
-    // Obtener fotos aleatorias de tu colección de Unsplash
+    // Obtener fotos aleatorias de la colección "nErL9-XsjU8" de Unsplash para las tarjetas de film
     fetch('https://api.unsplash.com/collections/nErL9-XsjU8/photos?client_id=' + accessKey + '&order_by=')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`Error HTTP! Estado: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        data.forEach((photo, index) => {
-            addImage(photo, index);
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error HTTP! Estado: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Iterar sobre las fotos y agregarlas a las tarjetas de film
+            data.forEach((photo, index) => {
+                addImage(photo, index);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener fotos de la colección:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error al obtener fotos de la colección:', error);
-    });
 });
--- js/unplash.js
+++ js/unplash.js
@@ -1,18 +1,32 @@
+// Clave de acceso para la API de Unsplash
 const accessKey = '44Yh3j04VI0h73noLdqOWPkEjsTRqP17zwhwsVNc7W4';
+
+// URL de la API de Unsplash para fotos aleatorias
 const apiUrl = 'https://api.unsplash.com/photos/random';
+
+// Contenedor HTML donde se mostrarán las fotos
 const photoContainer = document.getElementById('photo-container');
+
+// Selector de categoría HTML
 const categorySelect = document.getElementById('category');
+
+// Número de página actual
 let currentPage = 1;
 
+// Función para cargar fotos aleatorias desde Unsplash
 function loadRandomPhotos() {
+    // Obtener la categoría seleccionada y el número de fotos a cargar
     const category = categorySelect.value;
     const numberOfPhotos = 3;
 
+    // Construir la URL de la API con los parámetros necesarios
     const url = `${apiUrl}?client_id=${accessKey}&query=${category}&count=${numberOfPhotos}&page=${currentPage}`;
 
+    // Realizar la solicitud fetch a la API de Unsplash
     fetch(url)
         .then(response => response.json())
         .then(data => {
+            // Crear HTML para cada foto recibida y mostrarlo en el contenedor
             const photosHtml = data.map(photo => {
                 const artistName = photo.user.name;
                 const artistWeb = photo.user.links.html;
@@ -31,25 +45,25 @@ function loadRandomPhotos() {
             photoContainer.innerHTML = photosHtml;
         })
         .catch(error => {
+            // Manejar errores en la carga de fotos
             console.error('Error al cargar las fotos:', error);
             photoContainer.innerHTML = 'Error al cargar las fotos.';
         });
 }
 
+// Función para cargar la siguiente página de fotos
 function loadNextPage() {
     currentPage++;
     loadRandomPhotos();
 }
 
-// Trigger the function when the page loads or when the category is changed
+// Llamar a la función al cargar la página o al cambiar la categoría
 loadRandomPhotos();
 categorySelect.addEventListener('change', () => {
     currentPage = 1;
     loadRandomPhotos();
 });
 
-// Implementación básica de botón para cargar la siguiente página
+// Implementación básica de un botón para cargar la siguiente página
 const loadMoreButton = document.getElementById('load-more-button');
 loadMoreButton.addEventListener('click', loadNextPage);
-
-
